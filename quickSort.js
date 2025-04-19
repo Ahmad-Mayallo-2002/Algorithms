26789ab8d0866d1cf1ce1734d1374c57d5e24a31
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2025-04-20 01:55:28 +0200

    Algorithms

--- quickSort.js
+++ quickSort.js
@@ -2,8 +2,8 @@ function quickSort(array) {
   if (array.length < 2) return array;
   let [left, right, pivot] = [[], [], array[array.length - 1]];
   for (let i = 0; i < array.length - 1; i++)
-    pivot > array[i] ? left.push(array[i]) : right.push(array[i]);
-  return [...quickSort(left), pivot, ...quickSort(right)];
+    (pivot > array[i] ? left : right).push(array[i]);
+  return quickSort(left).concat(pivot, ...quickSort(right));
 }
 
 console.log(quickSort([17, 7, 5, 23, 65, 87, -1, -12, 0]));
