00195f98aacde4d4cb43e36218f2938dcea180c9
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2025-04-20 01:52:43 +0200

    Algorithms

--- selectionSort.js
+++ selectionSort.js
@@ -5,9 +5,7 @@ function selectionSort(array) {
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++)
       if (array[minIndex] > array[j]) minIndex = j;
-    let temp = array[i];
-    array[i] = array[minIndex];
-    array[minIndex] = temp;
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
   }
 }
 
